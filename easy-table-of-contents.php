a8f560923ede183fa35e19faf32b7031376cc2ac
Steven A. Zahm <shazahm1@hotmail.com>
2018-01-29 12:06:17 -0500

    Update copyright to 2018.

--- easy-table-of-contents.php
+++ easy-table-of-contents.php
@@ -9,7 +9,7 @@
  * Text Domain: easy-table-of-contents
  * Domain Path: /languages
  *
- * Copyright 2017  Steven A. Zahm  ( email : helpdesk@connections-pro.com )
+ * Copyright 2018  Steven A. Zahm  ( email : helpdesk@connections-pro.com )
  *
  * Easy Table of Contents is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
