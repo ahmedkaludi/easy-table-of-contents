78f9630b8dda2f989d21ebe9334f4d640360001b
Steven A. Zahm <shazahm1@hotmail.com>
2018-05-04 11:37:13 -0400

    Prefix a few CSS classes in order to prevent collisions with theme's and other plugins.

--- easy-table-of-contents.php
+++ easy-table-of-contents.php
@@ -1037,7 +1037,7 @@ if ( ! class_exists( 'ezTOC' ) ) {
 
 						if ( ezTOC_Option::get( 'visibility' ) ) {
 
-							$html .= '<a class="pull-right btn btn-xs btn-default ez-toc-toggle"><i class="glyphicon ez-toc-icon-toggle"></i></a>';
+							$html .= '<a class="ez-toc-pull-right ez-toc-btn ez-toc-btn-xs ez-toc-btn-default ez-toc-toggle"><i class="ez-toc-glyphicon ez-toc-icon-toggle"></i></a>';
 						}
 
 						$html .= '</span>';
