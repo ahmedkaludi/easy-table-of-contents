c5bbfc44dfadfd88d2f866dd77ac6351c5dc1a3e
Steven A. Zahm <shazahm1@hotmail.com>
2018-05-25 12:47:27 -0400

    Introduce helper functions for devs.

--- includes/inc.functions.php
+++ includes/inc.functions.php
@@ -4,47 +4,93 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
- * Returns a HTML formatted string of the table of contents without the surrounding UL or OL
- * tags to enable the theme editor to supply their own ID and/or classes to the outer list.
+ * Get the current post's TOC list or supplied post's TOC list.
  *
- * There are three optional parameters you can feed this function with:
+ * @access public
+ * @since  2.0
  *
- *        - $content is the entire content with headings.  If blank, will default to the current $post
+ * @param int|null|WP_Post $post                 An instance of WP_Post or post ID. Defaults to current post.
+ * @param bool             $apply_content_filter Whether or not to apply `the_content` filter when processing post for headings.
  *
- *        - $link is the URL to prefix the anchor with.  If provided a string, will use it as the prefix.
- *        If set to true then will try to obtain the permalink from the $post object.
- *
- *        - $apply_eligibility bool, defaults to false.  When set to true, will apply the check to
- *        see if bit of content has the prerequisites needed for a TOC, eg minimum number of headings
- *        enabled post type, etc.
+ * @return string
  */
-//function toc_get_index( $content = '', $prefix_url = '', $apply_eligibility = FALSE ) {
-//
-//	global $wp_query, $tic;
-//
-//	$return  = '';
-//	$find    = $replace = array();
-//	$proceed = TRUE;
-//
-//	if ( ! $content ) {
-//		$post    = get_post( $wp_query->post->ID );
-//		$content = wptexturize( $post->post_content );
-//	}
-//
-//	if ( $apply_eligibility ) {
-//		if ( ! $tic->is_eligible() ) {
-//			$proceed = FALSE;
-//		}
-//	} else {
-//		$tic->set_option( array( 'start' => 0 ) );
-//	}
-//
-//	if ( $proceed ) {
-//		$return = $tic->extract_headings( $find, $replace, $content );
-//		if ( $prefix_url ) {
-//			$return = str_replace( 'href="#', 'href="' . $prefix_url . '#', $return );
-//		}
-//	}
-//
-//	return $return;
-//}
+function get_ez_toc_list( $post = NULL, $apply_content_filter = TRUE ) {
+
+	if ( ! $post instanceof WP_Post ) {
+
+		$post = get_post( $post );
+	}
+
+	$ezPost = new ezTOC_Post( $post );
+
+	if ( $apply_content_filter ) {
+
+		$ezPost->applyContentFilter()->process();
+
+	} else {
+
+		$ezPost->process();
+	}
+
+	return $ezPost->getTOCList();
+}
+
+/**
+ * Display the current post's TOC list or supplied post's TOC list.
+ *
+ * @access public
+ * @since  2.0
+ *
+ * @param null|WP_Post $post                 An instance of WP_Post
+ * @param bool         $apply_content_filter Whether or not to apply `the_content` filter when processing post for headings.
+ */
+function ez_toc_list( $post = NULL, $apply_content_filter = TRUE ) {
+
+	echo get_ez_toc_list( $post, $apply_content_filter );
+}
+
+/**
+ * Get the current post's TOC content block or supplied post's TOC content block.
+ *
+ * @access public
+ * @since  2.0
+ *
+ * @param int|null|WP_Post $post                 An instance of WP_Post or post ID. Defaults to current post.
+ * @param bool             $apply_content_filter Whether or not to apply `the_content` filter when processing post for headings.
+ *
+ * @return string
+ */
+function get_ez_toc_block( $post = NULL, $apply_content_filter = TRUE ) {
+
+	if ( ! $post instanceof WP_Post ) {
+
+		$post = get_post( $post );
+	}
+
+	$ezPost = new ezTOC_Post( $post );
+
+	if ( $apply_content_filter ) {
+
+		$ezPost->applyContentFilter()->process();
+
+	} else {
+
+		$ezPost->process();
+	}
+
+	return $ezPost->getTOC();
+}
+
+/**
+ * Display the current post's TOC content or supplied post's TOC content.
+ *
+ * @access public
+ * @since  2.0
+ *
+ * @param null|WP_Post $post                 An instance of WP_Post
+ * @param bool         $apply_content_filter Whether or not to apply `the_content` filter when processing post for headings.
+ */
+function ez_toc_block( $post = NULL, $apply_content_filter = TRUE ) {
+
+	echo get_ez_toc_block( $post, $apply_content_filter );
+}
