3b63a9afb384bb018326defbd11a2a60540c493a
Steven A. Zahm <shazahm1@hotmail.com>
2018-06-07 12:43:47 -0400

    Remove the ezTOC shortcodes when applying `the_content` filter.
    gh-13

--- includes/class.post.php
+++ includes/class.post.php
@@ -115,8 +115,20 @@ class ezTOC_Post {
 		 * Ensure the ezTOC content filter is not applied when running `the_content` filter.
 		 */
 		remove_filter( 'the_content', array( 'ezTOC', 'the_content' ), 100 );
+
+		/*
+		 * Ensure the ezTOC shortcodes are not processed when applying `the_content` filter
+		 * otherwise an infinite loop may occur.
+		 */
+		remove_shortcode( 'ez-toc' ) ;
+		remove_shortcode( 'toc' );
+
 		$this->post->post_content = apply_filters( 'the_content', $this->post->post_content );
+
 		add_filter( 'the_content', array( 'ezTOC', 'the_content' ), 100 );
+
+		add_shortcode( 'ez-toc', array( 'ezTOC', 'shortcode' ) );
+		add_shortcode( apply_filters( 'ez_toc_shortcode', 'toc' ), array( 'ezTOC', 'shortcode' ) );
 
 		return $this;
 	}
