d59c54e70d277195669d326ad19505be274c4517
Steven A. Zahm <shazahm1@hotmail.com>
2020-01-30 13:14:14 -0500

    Add compatibility filters to remove shortcodes for Connections and Striking theme to remove them from eligible TOC item content.
    gh-69

--- includes/inc.plugin-compatibility.php
+++ includes/inc.plugin-compatibility.php
@@ -17,3 +17,58 @@ add_filter(
 		return $toc_plugins;
 	}
 );
+
+/**
+ * Filter to remove Connections Business Directory related shortcodes from being processed as eligible TOC items.
+ *
+ * @since 2.0
+ */
+add_filter(
+	'ez_toc_strip_shortcodes_tagnames',
+	function( $tags_to_remove ) {
+
+		$shortcodes = array (
+			'connections_form',
+			'cn_multi_category_search',
+			'cn_widget',
+			'cn_carousel',
+			'connections_categories',
+			'connections_link_view',
+			'connections_link_edit',
+			'connections_login',
+			'siteshot',
+			'connections',
+			'upcoming_list',
+			'cn-mapblock',
+			'connections_vcard',
+			'connections_qtip',
+			'cn_thumb',
+			'cn_thumbr',
+		);
+
+		$tags_to_remove = array_merge( $tags_to_remove, $shortcodes );
+
+		return $tags_to_remove;
+	}
+);
+
+
+/**
+ * Filter to remove Striking theme related shortcodes from being processed as eligible TOC items.
+ *
+ * @since 2.0
+ */
+add_filter(
+	'ez_toc_strip_shortcodes_tagnames',
+	function( $tags_to_remove ) {
+
+		$shortcodes = array (
+			'iconbox',
+			'toggle',
+		);
+
+		$tags_to_remove = array_merge( $tags_to_remove, $shortcodes );
+
+		return $tags_to_remove;
+	}
+);
