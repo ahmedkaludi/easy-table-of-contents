88ea6f10ab7add65250d25985d3c1b7c3163913c
Steven A. Zahm <shazahm1@hotmail.com>
2020-02-26 14:41:45 -0500

    No need to check the current filter since this is already being done in the `maybeApplyTheContentFilter()` method.

--- easy-table-of-contents.php
+++ easy-table-of-contents.php
@@ -366,17 +366,18 @@ if ( ! class_exists( 'ezTOC' ) ) {
 		 */
 		public static function is_eligible( $post ) {
 
-			global $wp_current_filter;
+			//global $wp_current_filter;
 
 			if ( empty( $post ) || ! $post instanceof WP_Post ) {
 				return false;
 			}
 
+			// This can likely be removed since it is checked in maybeApplyTheContentFilter().
 			// Do not execute if root filter is one of those in the array.
-			if ( in_array( $wp_current_filter[0], array( 'get_the_excerpt', 'wp_head' ), true ) ) {
-
-				return false;
-			}
+			//if ( in_array( $wp_current_filter[0], array( 'get_the_excerpt', 'wp_head' ), true ) ) {
+			//
+			//	return false;
+			//}
 
 			if ( has_shortcode( $post->post_content, apply_filters( 'ez_toc_shortcode', 'toc' ) ) ||
 			     has_shortcode( $post->post_content, 'ez-toc' ) ) {
