c897ae7449611b506ef8206ae51e6a855544bb04
Steven A. Zahm <shazahm1@hotmail.com>
2020-04-02 12:43:02 -0400

    Use `esc_attr()` instead of `esc_url()` for the anchor href because valid id attribute characters would cause it to return an empty href which cause a nonworking link.

--- includes/class.post.php
+++ includes/class.post.php
@@ -1307,7 +1307,7 @@ class ezTOC_Post {
 
 		return sprintf(
 			'<a class="ez-toc-link ez-toc-heading-' . $count . '" href="%1$s" title="%2$s">%3$s</a>',
-			esc_url( $this->createTOCItemURL( $id, $page ) ),
+			esc_attr( $this->createTOCItemURL( $id, $page ) ),
 			esc_attr( strip_tags( $title ) ),
 			$title
 		);
