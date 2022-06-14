64d50a119c803785cb5eecc2aa7056c6378c01d1
Syed Zabiuddin <syedzabi332@gmail.com>
2022-06-14 13:05:15 +0530

    #195 issue fixed

--- easy-table-of-contents.php
+++ easy-table-of-contents.php
@@ -266,7 +266,9 @@ if ( ! class_exists( 'ezTOC' ) ) {
 
 				$js_vars['width'] = esc_js( $width );
 			}else{
-				$js_vars['visibility_hide_by_default'] = ezTOC_Option::get( 'visibility_hide_by_default' ) ? true : false;
+				if(ezTOC_Option::get( 'visibility' )){
+					$js_vars['visibility_hide_by_default'] = ezTOC_Option::get( 'visibility_hide_by_default' ) ? true : false;
+				}
 			}
 
 			$offset = wp_is_mobile() ? ezTOC_Option::get( 'mobile_smooth_scroll_offset', 0 ) : ezTOC_Option::get( 'smooth_scroll_offset', 30 );
