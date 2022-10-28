1f5addd52c6817d93aeb814bbe9f16d22453255b
Muslim Ahmad <muslimahmadkhan@gmail.com>
2022-10-28 19:24:36 +0530

    #308 Icon not fitted last time so now revised code and resolved & now running smooth

--- includes/inc.functions.php
+++ includes/inc.functions.php
@@ -107,7 +107,7 @@ function ez_toc_woo_category_desc() {
   $desc = $term_object->description;
   preg_match_all( '@<h1.*?>(.*?)<\/h1>@', $desc, $matches );
   $array = $matches[1];
-  $container = '<div id="ez-toc-container" class="counter-hierarchy counter-decimal ez-toc-grey"><div class="ez-toc-title-container"><p class="ez-toc-title">' . esc_html_e('Table of Contents', 'easy-table-of-contents') . '</p><span class="ez-toc-title-toggle"><a class="ez-toc-pull-right ez-toc-btn ez-toc-btn-xs ez-toc-btn-default ez-toc-toggle" style="display: inline;">' . ezTOC::getTOCToggleIcon() . '</a></span></div><nav><ul class="ez-toc-list">';
+  $container = '<div id="ez-toc-container" class="counter-hierarchy counter-decimal ez-toc-grey"><div class="ez-toc-title-container"><p class="ez-toc-title">' . esc_html_e('Table of Contents', 'easy-table-of-contents') . '</p><span class="ez-toc-title-toggle"><a class="ez-toc-pull-right ez-toc-btn ez-toc-btn-xs ez-toc-btn-default ez-toc-toggle" style="display: flex;">' . ezTOC::getTOCToggleIcon() . '</a></span></div><nav><ul class="ez-toc-list">';
     foreach ( $array as $val ) { 
       $vals .= '<li><a class="anchor" href="#'.$val.'">'.$val.'</a></li>';
       $desc = preg_replace('/<h1>(.*?)<\/h1>/', "<h1 id='$1'>$1</h1>", $desc);
