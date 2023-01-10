675c479c07f1e1549461511ab5a48de15f333011
Muslim Ahmad <muslimahmadkhan@gmail.com>
2023-01-10 16:03:24 +0530

    #394 Resolved Now

--- easy-table-of-contents.php
+++ easy-table-of-contents.php
@@ -392,24 +392,7 @@ INLINESCROLLCSS;
             wp_add_inline_style( 'ez-toc', $inlineScrollCSS );
             
             $inlineScrollJS = <<<INLINESCROLLJS
-function ezTocScrollScriptJS() {
-    document.querySelectorAll('.ez-toc-section').forEach(span => {
-//        console.log(span.getAttribute('id'));
-        span.setAttribute('ez-toc-data-id', '#' + decodeURI(span.getAttribute('id')));
-	});
-    document.querySelectorAll('a.ez-toc-link').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-//            console.log('[id="' + this.getAttribute('href') + '"]');
-//            console.log(document.getElementById('[id="' + this.getAttribute('href') + '"]'));
-            
-            document.querySelector('[ez-toc-data-id="' + decodeURI(this.getAttribute('href')) + '"]').scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
-};           
-document.addEventListener('DOMContentLoaded', ezTocScrollScriptJS, false);
+function ezTocScrollScriptJS(){document.querySelectorAll(".ez-toc-section").forEach(t=>{t.setAttribute("ez-toc-data-id","#"+decodeURI(t.getAttribute("id")))}),document.querySelectorAll("a.ez-toc-link").forEach(t=>{t.addEventListener("click",function(t){t.preventDefault();var e=this.getAttribute("href");document.querySelector('[ez-toc-data-id="'+decodeURI(this.getAttribute("href"))+'"]').scrollIntoView({behavior:"smooth"}),history.replaceState("data","title",location.origin+location.pathname+e)})})}document.addEventListener("DOMContentLoaded",ezTocScrollScriptJS,!1);
 INLINESCROLLJS;
             wp_register_script( 'ez-toc-scroll-scriptjs', '', array(), ezTOC::VERSION );
             wp_enqueue_script( 'ez-toc-scroll-scriptjs', '', array(), ezTOC::VERSION );
