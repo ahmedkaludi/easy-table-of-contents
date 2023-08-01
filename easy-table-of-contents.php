99229ab905d7ae474e871798d274cde3b981b60a
Sanjeev Kumar <sanjeevsetu@gmail.com>
2023-08-01 11:18:58 +0530

    #532

--- easy-table-of-contents.php
+++ easy-table-of-contents.php
@@ -1021,15 +1021,15 @@ INLINESTICKYTOGGLEJS;
 					/**
 					 * @link https://wordpress.org/support/topic/restrict-path-logic-does-not-work-correctly?
 					 */
-					if ( false === strpos(esc_url($_SERVER['REQUEST_URI']), ezTOC_Option::get( 'restrict_path' ) ) ) {
+					if ( isset($_SERVER['REQUEST_URI']) && false !== strpos( ezTOC_Option::get( 'restrict_path' ), $_SERVER['REQUEST_URI'] ) ) {
 
 						Debug::log( 'is_restricted_path', 'In restricted path, post not eligible.', ezTOC_Option::get( 'restrict_path' ) );
-						return true;
+						return false;
 
 					} else {
 
 						Debug::log( 'is_not_restricted_path', 'Not in restricted path, post is eligible.', ezTOC_Option::get( 'restrict_path' ) );
-						return false;
+						return true;
 					}
 
 				} else {
