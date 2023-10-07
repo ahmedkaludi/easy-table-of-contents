c072a2901ece9a342170bd9ef61fafba861ba1c3
Akshay Wali <akshaywali@live.in>
2023-10-07 18:04:14 +0530

    Toggle UI issues  #604

--- includes/class.post.php
+++ includes/class.post.php
@@ -1475,9 +1475,10 @@ class ezTOC_Post {
 				$toc_icon = apply_filters('ez_toc_modify_icon',$toc_icon);
 		     }				
 			if ( ezTOC_Option::get( 'visibility_on_header_text' ) ) {		
-				$html .= '<label for="ez-toc-cssicon-toggle-item-' . $cssIconID . '" class=" ez-toc-cssicon-toggle-label">' .$header_label. $toc_icon . '</label><input type="checkbox" ' . $inputCheckboxExludeStyle . ' id="ez-toc-cssicon-toggle-item-' . $cssIconID . '" '.$toggle_view.' />';
+				$html .= '<label for="ez-toc-cssicon-toggle-item-' . $cssIconID . '" class="ez-toc-cssicon-toggle-label">' .$header_label. $toc_icon . '</label><input type="checkbox" ' . $inputCheckboxExludeStyle . ' id="ez-toc-cssicon-toggle-item-' . $cssIconID . '" '.$toggle_view.' />';
 			}else{
-				$html .= $header_label.'<label for="ez-toc-cssicon-toggle-item-' . $cssIconID . '" class=" ez-toc-cssicon-toggle-label">' . $toc_icon . '</label><input type="checkbox" ' . $inputCheckboxExludeStyle . ' id="ez-toc-cssicon-toggle-item-' . $cssIconID . '" '.$toggle_view.' aria-label="Toggle" />';
+				$html .= '<div class="ez-toc-cssicon-toggle-label">'.$header_label.'<label for="ez-toc-cssicon-toggle-item-' . $cssIconID . '">' . $toc_icon . '</label></div><input type="checkbox" ' . $inputCheckboxExludeStyle . ' id="ez-toc-cssicon-toggle-item-' . $cssIconID . '" '.$toggle_view.' aria-label="Toggle" />';
+				//$html .= $header_label.'<label for="ez-toc-cssicon-toggle-item-' . $cssIconID . '" class="ez-toc-cssicon-toggle-label">' . $toc_icon . '</label><input type="checkbox" ' . $inputCheckboxExludeStyle . ' id="ez-toc-cssicon-toggle-item-' . $cssIconID . '" '.$toggle_view.' aria-label="Toggle" />';
 			}
 					
 		}		
