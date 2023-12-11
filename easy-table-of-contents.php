12da41a41f9baff7fbc4e41f278f25bfe0854de0
Rkroy <43204388+Rkroy@users.noreply.github.com>
2023-12-11 19:57:06 +0530

    #623

--- easy-table-of-contents.php
+++ easy-table-of-contents.php
@@ -593,7 +593,6 @@ INLINEWPBAKERYJS;
 				$css .= 'div#ez-toc-container .ez-toc-title {font-weight: ' . ezTOC_Option::get( 'title_font_weight', 500 ) . ';}';
 				$css .= 'div#ez-toc-container ul li {font-size: ' . ezTOC_Option::get( 'font_size' ) . ezTOC_Option::get( 'font_size_units' ) . ';}';
 				$css .= 'div#ez-toc-container nav ul ul li {font-size: ' . ezTOC_Option::get( 'child_font_size' ) . ezTOC_Option::get( 'child_font_size_units' ) . ';}';
-				$css .= 'div#ez-toc-container nav ul ul li ul li {font-size: ' . ezTOC_Option::get( 'child_font_size' ) . ezTOC_Option::get( 'child_font_size_units' ) . '!important;}';
 
 				if ( ezTOC_Option::get( 'theme' ) === 'custom' || ezTOC_Option::get( 'width' ) != 'auto' ) {
 
