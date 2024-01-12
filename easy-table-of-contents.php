18696dbb8db84d64de95fa83f28f622ecc3f9f44
Rkroy <43204388+Rkroy@users.noreply.github.com>
2024-01-12 17:17:37 +0530

    Changelog and Vernum added.

--- easy-table-of-contents.php
+++ easy-table-of-contents.php
@@ -3,7 +3,7 @@
  * Plugin Name: Easy Table of Contents
  * Plugin URI: https://tocwp.com/
  * Description: Adds a user friendly and fully automatic way to create and display a table of contents generated from the page content.
- * Version: 2.0.61
+ * Version: 2.0.62
  * Author: Magazine3
  * Author URI: https://tocwp.com/
  * Text Domain: easy-table-of-contents
@@ -26,7 +26,7 @@
  * @package  Easy Table of Contents
  * @category Plugin
  * @author   Magazine3
- * @version  2.0.61
+ * @version  2.0.62
  */
 
 use Easy_Plugins\Table_Of_Contents\Debug;
@@ -50,7 +50,7 @@ if ( ! class_exists( 'ezTOC' ) ) {
 		 * @since 1.0
 		 * @var string
 		 */
-		const VERSION = '2.0.61';
+		const VERSION = '2.0.62';
 
 		/**
 		 * Stores the instance of this class.
