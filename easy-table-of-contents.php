a26b6371365555fb476e90956e42a64d7280c4ab
Akshay Wali <akshaywali@live.in>
2025-08-20 19:08:08 +0530

    Update easy-table-of-contents.php

--- easy-table-of-contents.php
+++ easy-table-of-contents.php
@@ -3,7 +3,7 @@
  * Plugin Name: Easy Table of Contents
  * Plugin URI: https://tocwp.com/
  * Description: Adds a user friendly and fully automatic way to create and display a table of contents generated from the page content.
- * Version: 2.0.75
+ * Version: 2.0.76
  * Author: Magazine3
  * Author URI: https://tocwp.com/
  * Text Domain: easy-table-of-contents
@@ -28,7 +28,7 @@
  * @package  Easy Table of Contents
  * @category Plugin
  * @author   Magazine3
- * @version  2.0.75
+ * @version  2.0.76
  */
 
 use Easy_Plugins\Table_Of_Contents\Debug;
@@ -52,7 +52,7 @@ if ( ! class_exists( 'ezTOC' ) ) {
 		 * @since 1.0
 		 * @var string
 		 */
-		const VERSION = '2.0.75';
+		const VERSION = '2.0.76';
 
 		/**
 		 * Stores the instance of this class.
